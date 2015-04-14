5612652234a4eb5fc82639cb14af44a6873070b7
Samuel John <github@samueljohn.de>
2015-04-14 16:15:50 -0400

    fix setup_git_strip_notebook_output_filter.sh path
    Is there any better way to get the current working dir when
    in the git repo?

--- setup_git_strip_notebook_output_filter.sh
+++ setup_git_strip_notebook_output_filter.sh
@@ -1,3 +1,3 @@
 git config core.attributesfile ~/.gitattributes
-git config filter.stripoutput.clean $(CWD)/strip_notebook_output.py
+git config filter.stripoutput.clean $PWD/strip_notebook_output.py
 git config filter.stripoutput.smudge cat
